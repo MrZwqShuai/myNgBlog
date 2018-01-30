7014784048663f0f800d6c46c039d61b6650693e
MrZwqShuai <zwq1098621287@yahoo.com>
2018-01-30 22:28:26 +0800

    zwq

--- src/app/pages/speacial-column/column-right/column-right.component.ts
+++ src/app/pages/speacial-column/column-right/column-right.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColumnRightComponent implements OnInit {
 
-  private hotNotes: Array<object> = [{
+  private hotNotes: Array<Object> = [{
     title: '程序生涯'
   }, {
     title: '篮球生涯'
@@ -15,13 +15,27 @@ export class ColumnRightComponent implements OnInit {
     title: '游戏生涯'
   }];
 
-  private news: Array<object> = [{
+  private news: Array<Object> = [{
     title: '微软可能会在智能耳机上与苹果AirPods竞争'
   }, {
     title: '消息称SAP/McAfee/赛门铁克正让俄罗斯监管机'
   }, {
     title: '《绝地求生》Xbox One版销量已达400万 官方将免费送3万BP币'
-  }]
+  }];
+
+  private channels: Array<Object> = [{
+    title: 'Java'
+  }, {
+    title: 'Javascript'
+  }, {
+    title: 'NodeJs'
+  }, {
+    title: 'Angular'
+  }, {
+    title: 'Vue'
+  }, {
+    title: 'MYSQL'
+  }];
 
   constructor() { }
 
