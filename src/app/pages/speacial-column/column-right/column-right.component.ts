0b41f1df149f6c1653919f26d1fa9aefb067e520
wxp <310082609@qq.com>
2018-02-01 17:35:17 +0800

    zwq

--- src/app/pages/speacial-column/column-right/column-right.component.ts
+++ src/app/pages/speacial-column/column-right/column-right.component.ts
@@ -33,78 +33,16 @@ export class ColumnRightComponent implements OnInit {
     'MYSQL'
   ];
 
+  // 鼠标按下位移
+  private startX: number = 0;
+  private startY: number = 0;
+  private disX: number;
+  private disY: number;
+
   constructor(private _speacialColumnServiceService: SpeacialColumnServiceService) { }
 
   ngOnInit() {
-    // this.getAllMeta();
-    
-        move();
-
-        clickBox();
-
-        //鼠标按下且移动时触发，   
-
-        function move() {
-            var body = document.querySelector("body");
-            var box = document.querySelector(".box");
-            var xNum = document.querySelector(".xNum");
-            var yNum = document.querySelector(".yNum");
-            var x = 0, y = 0, z = 0;
-            var xx = 0, yy = 0;
-            var xArr = [], yArr = [];
-            window.onmousedown = function (e) {//鼠标按下事件   
-                body.style.cursor = 'url("img/closedhand1.png"),auto';
-                xArr[0] = e.clientX / 2;//获取鼠标点击屏幕时的坐标   
-                yArr[0] = e.clientY / 2;
-                window.onmousemove = function (e) {//鼠标移动事件————当鼠标按下且移动时触发   
-                    xArr[1] = e.clientX / 2;//获取鼠标移动时第一个点的坐标   
-                    yArr[1] = e.clientY / 2;
-                    yy += xArr[1] - xArr[0];//获得鼠标移动的距离   
-                    xx += yArr[1] - yArr[0];
-                    xNum.value = xx + "°";//将所获得距离数字赋值给input显示旋转角度   
-                    yNum.value = yy + "°";
-                    //将旋转角度写入transform中   
-                    box.style.transform = "rotatex(" + xx + "deg) rotatey(" + yy + "deg) rotatez(0deg)scale3d(0.7,0.7,0.7)";
-                    xArr[0] = e.clientX / 2;
-                    yArr[0] = e.clientY / 2;
-                }
-
-            };
-            window.onmouseup = function () {//鼠标抬起事件————用于清除鼠标移动事件，   
-                body.style.cursor = 'url("img/openhand1.png"),auto';
-                window.onmousemove = null;
-            }
-        }
-        //点击事件、负责立方体盒子的六面伸展   
-        function clickBox() {
-            var btn = document.querySelector(".open");
-            var box = document.querySelector(".box");
-            var son = box.children;
-            var value = 0;
-            //存储立方体散开时的transform参数   
-            var arr0 = ["rotatey(-90deg) translatex(-100px)", "rotatey(90deg) translatex(100px)", "rotatex(90deg) translatey(-100px)","rotatex(-90deg) translatey(100px)", "translatez(-100px)", "translatez(100px)"];
-            //存储立方体合并时的transform参数   
-            var arr1 = ["rotatey(-90deg) translatex(-100px)translatez(100px)", "rotatey(90deg) translatex(100px)translatez(100px)","rotatex(90deg) translatey(-100px)translatez(100px)", "rotatex(-90deg) translatey(100px)translatez(100px)", "translatez(-200px)", "translatez(200px)"];
-            btn.onclick = function () {
-                if (value == 0) {
-                    value = 1;
-                    btn.value = "点击合并";
-                    for (var i = 0; i < arr1.length; i++) {
-                        son[i].style.transform = arr1[i];
-                        console.log(son[i])
-                    }
-                } else if (value == 1) {
-                    value = 0;
-                    btn.value = "点击散开";
-                    for (var j = 0; j < arr0.length; j++) {
-                        son[j].style.transform = arr0[j];
-                        console.log(j);
-
-                    }
-                }
-            };
-        }
-
+    this.getAllMeta();
   }
 
   getAllMeta() {
@@ -115,4 +53,20 @@ export class ColumnRightComponent implements OnInit {
       })
   }
 
+  mouseDown(e: MouseEvent) {
+    this.startX = e.clientX;
+    this.startY = e.clientY;
+  }
+
+  mouseMove(e: MouseEvent) {
+      this.disX = e.clientX - this.startX;
+      this.disY = e.clientY - this.startY;
+      document.querySelector('.channel-sz').style.transform = `rotateX(${this.disX}deg)`;
+      document.querySelector('.channel-sz').style.transform = `rotateY(${this.disY}deg)`;
+  }
+
+  mouseUp(e: MouseEvent) {
+    // this.mouseMove = null;
+    alert(0)
+  }
 }
