a353b441f3615c95f52912ea39ad2c6f4e9ff593
wxp <310082609@qq.com>
2018-02-02 18:20:29 +0800

    zwq

--- src/app/pages/speacial-column/column-right/column-right.component.ts
+++ src/app/pages/speacial-column/column-right/column-right.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, Renderer, Renderer2 } from '@angular/core';
 import { SpeacialColumnServiceService } from '../../service/speacial-column.service';
 
 @Component({
@@ -30,43 +30,116 @@ export class ColumnRightComponent implements OnInit {
     'NodeJs',
     'Angular',
     'Vue',
-    'MYSQL'
+    'MYSQL',
+    '后端',
+    '前端',
+    'Python',
+    'React',
+    'C++',
+    'C#'
   ];
+
+  private metaData: String[];
+
+  //控制mouse事件的变量
+  private forMouseDown: boolean = true;
+  private forMouseMove: boolean = false;
+  private forMouseUp: boolean = false;
 
   // 鼠标按下位移
   private startX: number = 0;
   private startY: number = 0;
-  private disX: number;
-  private disY: number;
+  private disX: number = 0;
+  private disY: number = 0;
 
-  constructor(private _speacialColumnServiceService: SpeacialColumnServiceService) { }
+  // 获取立方体dom
+  private cubeMeta: HTMLElement;
+  private cubePlane: Array<HTMLElement>;
+
+  constructor(private _speacialColumnServiceService: SpeacialColumnServiceService, private _elementRef: ElementRef, 
+    private _renderer: Renderer, private _renderer2: Renderer2) { }
 
   ngOnInit() {
+    this.metaData =this.channels;
+    this.cubeMeta = this._elementRef.nativeElement.querySelector('.channel-sz');
+    console.log(2, this.cubePlane);
     this.getAllMeta();
+    this.listenerWindowKeyUp();
+  }
+  ngAfterViewInit() {
+    this.cubePlane = this._elementRef.nativeElement.querySelectorAll('.tangent-plane');
   }
 
   getAllMeta() {
     this._speacialColumnServiceService.getAllMeta()
       .subscribe((metaData: any) => {
         this.channels = metaData.data.slice(0, 6);
-        console.log(this.channels, '222');
       })
   }
 
   mouseDown(e: MouseEvent) {
-    this.startX = e.clientX;
-    this.startY = e.clientY;
+    this.forMouseMove = true;
+    this.startX = e.clientX/2;
+    this.startY = e.clientY/2;
+    this._renderer.setElementStyle(this.cubeMeta, 'transition', 'all .2s ease');
   }
 
   mouseMove(e: MouseEvent) {
-      this.disX = e.clientX - this.startX;
-      this.disY = e.clientY - this.startY;
-      document.querySelector('.channel-sz').style.transform = `rotateX(${this.disX}deg)`;
-      document.querySelector('.channel-sz').style.transform = `rotateY(${this.disY}deg)`;
-  }
+    if (this.forMouseMove) {
+      let xx = e.clientX/2;
+      let yy = e.clientY/2;
+      this.disX += (xx - this.startX)*1.5;
+      this.disY += (yy - this.startY)*1.5;
+      this._renderer.setElementStyle(this.cubeMeta, 'transform', `rotateX(${this.disX}deg) rotateY(${this.disY}deg) rotateZ(0deg)`);  
+      this.startX = e.clientX/2;
+      this.startY = e.clientY/2;
+    }
+    this.forMouseUp = true;
+  } 
 
   mouseUp(e: MouseEvent) {
     // this.mouseMove = null;
-    alert(0)
+    if(this.forMouseUp) {
+      this.forMouseMove = false;
+      console.log('松开鼠标');
+    }
+  }
+  
+  changeMeta():void {
+    //清除move事件
+    this.forMouseMove = false;
+    this._renderer.setElementStyle(this.cubeMeta, 'transition', 'all 1s ease');
+    this._renderer.setElementStyle(this.cubeMeta, 'transform', `rotateY(${this.getRadomAngle(90)}deg) rotateY(${this.getRadomAngle(180)}deg) rotateZ(${this.getRadomAngle(90)}deg)`);
+    this.getRadomMeta();
+    // this.addMetaChildrenClass();
+    this.startX = this.getRadomAngle(90);
+    this.startY = this.getRadomAngle(180);
+  }
+
+  addMetaChildrenClass() {
+    this.cubePlane.forEach((e: HTMLElement) => {
+      this._renderer2.setStyle(e, 'transform', 'rotateX(100px) rotateY(100px)');
+    })
+  }
+
+  getRadomMeta() {
+    // 换一批标签
+    let randomNum = Math.floor(Math.random()*6);
+    this.channels = this.metaData.slice(randomNum, randomNum+6);
+  }
+
+  // 获得随机角度
+  getRadomAngle(baseAngle: number) {
+    return Math.floor(baseAngle + Math.random()*360);
+  }
+
+  // 监听全局keyup事件
+  listenerWindowKeyUp() {
+    window.onmouseup = (e: MouseEvent):void => {
+      if(this.forMouseUp) {
+        this.forMouseMove = false;
+        console.log('松开鼠标');
+      }
+    }
   }
 }
