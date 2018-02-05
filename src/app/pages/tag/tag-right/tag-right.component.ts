083cdd371c25248109b1ee487eed1a9ba100c7d4
MrZwqShuai <zwq1098621287@yahoo.com>
2018-02-05 23:08:53 +0800

    zwq

--- src/app/pages/tag/tag-right/tag-right.component.ts
+++ src/app/pages/tag/tag-right/tag-right.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tag-right',
   templateUrl: './tag-right.component.html',
-  styleUrls: ['./tag-right.component.css']
+  styleUrls: ['./tag-right.component.scss']
 })
 export class TagRightComponent implements OnInit {
 
