67d8f5864c97e10d4a52f96c0e0564bba2a94aae
MrZwqShuai <zwq1098621287@yahoo.com>
2018-02-06 22:44:54 +0800

    zwq

--- src/app/pages/editor/editor.component.ts
+++ src/app/pages/editor/editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-editor',
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorComponent implements OnInit {
 
-  constructor() { }
+  private menuCollection: Array<Object> = [{
+    name: '加粗',
+    feature: 'bold'
+  }, {
+    name: '斜体'
+  }, {
+    name: 'h1'
+  }, {
+    name: 'h2'
+  }, {
+    name: '链接'
+  }];
+  private editor: HTMLElement;
+
+  constructor() { 
+  }
 
   ngOnInit() {
+    this.editor = <HTMLElement>document.querySelector(".edit-area");
+  }
+
+  
+  changeInput(feature) {
+    // let selection = window.getSelection();
+    // let range = selection.getRangeAt(0);
+    // console.log(range);
+    console.log(this.editor);
+    let result = document.execCommand("Cut", false, null);
+    console.log('是否成功', result);
   }
 
 }
